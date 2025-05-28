a9d3fbbba7d394aac4cce272ea73bf47f64d3ac1
anamaydev <anamaydev@gmail.com>
2025-05-28 21:01:34 +0530

    fix: use separate navs for mobile and desktop to avoid layout flash

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import { useState } from "react";
 
 import logo from '../../assets/images/logo.svg'
 import menu from '../../assets/images/icon-menu.svg'
@@ -7,42 +7,48 @@ import menuClose from '../../assets/images/icon-menu-close.svg'
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 770) {
-        setIsOpen(false);
-      }
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  function NavList(){
+    const navPageArray = [
+      {name: 'Home', href:'#'},
+      {name: 'New', href:'#'},
+      {name: 'Popular', href:'#'},
+      {name: 'Trending', href:'#'},
+      {name: 'Categories', href:'#'}
+    ]
+
+    return navPageArray.map(page=> <li><a href={page.href}>{page.name}</a></li>)
+  }
 
   return (
     <header className="flex justify-between items-center">
       {/* logo */}
       <img src={logo} alt="World News logo" className="h-3.5 lg:h-5 transition-h duration-500" />
 
-      {/* backdrop shadow */}
-      {isOpen && <div className={`md:hidden absolute inset-0 bg-navy-950/25 z-10`}></div>}
-      <nav className="">
-        <ul
-          className={`
-            flex gap-5 text-preset-6 bg-white text-navy-600
-            max-md:flex-col max-md:fixed max-md:z-20 max-md:pt-18 max-md:pl-4.5 max-sm:pl-2.5
-            max-md:right-0 max-md:inset-y-0 max-md:left-[60%] max-sm:left-[30%]
-            transform transition-transform duration-500 ease-in-out
-            ${isOpen ? 'max-md:translate-x-0' : 'max-md:translate-x-full'}
-          `}
-        >
-          <li><a href="#">Home</a></li>
-          <li><a href="#">New</a></li>
-          <li><a href="#">Popular</a></li>
-          <li><a href="#">Trending</a></li>
-          <li><a href="#">Categories</a></li>
+      {/* backdrop shadow*/}
+      {isOpen && <div className="md:hidden absolute inset-0 z-10 bg-navy-950/25"></div>}
+
+      {/* mobile and tablet navbar */}
+      <nav>
+        <ul className={`
+          text-preset-5 text-navy-950 bg-white
+          flex flex-col gap-3
+          fixed z-20 inset-y-0 right-0 left-[30%] sm:left-[60%]
+          pt-18 pl-2.5 sm:pl-4.5
+          md:hidden
+          transform transition-all duration-1000 ease-in-out
+          ${isOpen ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0'}`
+        }>
+          <NavList/>
+        </ul>
+
+        {/* desktop navbar */}
+        <ul className="hidden text-preset-6 md:flex gap-5 opacity-0 md:opacity-100">
+          <NavList/>
         </ul>
       </nav>
-      <button className="max-md:relative md:hidden z-30 h-5 w-5 flex justify-center items-center">
-        <img src={isOpen ? menuClose : menu} alt="" className="" onClick={()=>setIsOpen(!isOpen)}/>
+
+      <button className="max-md:relative md:hidden z-30 h-5 w-5 flex justify-center items-center cursor-pointer">
+        <img src={isOpen ? menuClose : menu} alt="" onClick={()=>setIsOpen(!isOpen)}/>
       </button>
     </header>
   )
